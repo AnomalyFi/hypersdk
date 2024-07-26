8f6fcda98ea30f83c8b517c4c9a36fb2e65d250c
bianyuanop <chen.me.nan@gmail.com>
2024-07-27 01:52:10 +0800

    skip failed txns from adding it to NMT tree

--- chain/block.go
+++ chain/block.go
@@ -260,12 +260,14 @@ func (b *StatelessBlock) initializeBuilt(
 
 	for i := 0; i < len(b.Txs); i++ {
 		tx := b.Txs[i]
-		// txResult := results[i]
 		txID := tx.ID()
 
+		txResult := results[i]
+		if !txResult.Success {
+			continue
+		}
 		for j := 0; j < len(tx.Actions); j++ {
 
-			txResult := results[i]
 			nID := tx.Actions[j].NMTNamespace()
 			nmtNSs = append(nmtNSs, nID)
 
